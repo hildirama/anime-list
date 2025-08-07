3a7d2976b691a6e8940cf208bec15bc0cf25c5c8
hildirama <hildirama123@gmail.com>
2025-08-07 21:16:30 +0700

    masih stuck :(

--- src/app/components/AnimeList/index.js
+++ src/app/components/AnimeList/index.js
@@ -1,11 +1,11 @@
 import Image from "next/image";
 
-const AnimeList = ({ title, images }) => {
+const AnimeList = ({title, images}) => {
   return (
-    <div className="bg-sky-500">
+    <div className="">
       <Image src={images} alt="..." width={600} height={600} />
       <h3>{title}</h3>
     </div>
-  );
-};
-export default AnimeList;
+  )
+}
+export default AnimeList
--- src/app/page.js
+++ src/app/page.js
@@ -1,21 +1,18 @@
-import AnimeList from "@/app/components/listAnime";
+import AnimeList from "@/app/components/AnimeList"
 
-const home = async () => {
-  const response = await fetch(
-    `${process.env.NEXT_PUBLIC_API_BASE_URL}/top/anime}`
-  );
-  const anime = await response.json();
+const Home = async () => {
+
+  const response = await fetch(`${process.env.NEXT_PUBLIC_API_BASE_URL}/top/anime`)
+  const anime = await response.json()
 
   return (
     <div>
       <h1>MOST POPULAR</h1>
-      {anime.data.map((data) => {
-        return (
-          <AnimeList title={data.title} images={data.images.webp.image_url} />
-        );
+      {anime.data.map(data => {
+      return <AnimeList title={data.title} images={data.images.webp.image_url}/>
       })}
     </div>
-  );
-};
+  )  
+}
 
-export default home;
+export default Home
