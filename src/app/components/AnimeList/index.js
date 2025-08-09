61980d8ede983510f4e4dece2933121e0a883d09
hildirama <hildirama123@gmail.com>
2025-08-09 16:42:07 +0700

    alhamdulillah

--- src/app/components/AnimeList/index.js
+++ src/app/components/AnimeList/index.js
@@ -3,7 +3,7 @@ import Image from "next/image";
 const AnimeList = ({title, images}) => {
   return (
     <div className="">
-      <Image src={images} alt="..." width={600} height={600} />
+      <Image src={images} alt="..." width={400} height={300} />
       <h3>{title}</h3>
     </div>
   )
--- src/app/page.js
+++ src/app/page.js
@@ -8,9 +8,11 @@ const Home = async () => {
   return (
     <div>
       <h1>MOST POPULAR</h1>
+      <div className="grid grid-cols-3 gap-4">
       {anime.data.map(data => {
-      return <AnimeList key={data} title={data.title} images={data.images.webp.image_url}/>
+      return <AnimeList key={data.mal_id} title={data.title} images={data.images.webp.image_url}/>
       })}
+      </div>
     </div>
   )  
 }
