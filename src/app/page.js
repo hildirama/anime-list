c30706b0b41ca81ae42c06c1265d27455d62e023
hildirama <hildirama123@gmail.com>
2025-08-10 18:00:48 +0700

    design interface

--- src/app/page.js
+++ src/app/page.js
@@ -1,16 +1,24 @@
 import AnimeList from "@/app/components/AnimeList"
+import Link from "next/link"
 
 const Home = async () => {
 
-  const response = await fetch(`${process.env.NEXT_PUBLIC_API_BASE_URL}/top/anime`)
+  const response = await fetch(`${process.env.NEXT_PUBLIC_API_BASE_URL}/top/anime?limit=8`)
   const anime = await response.json()
 
   return (
     <div>
-      <h1>MOST POPULAR</h1>
-      <div className="grid grid-cols-3 gap-4">
+      <div className="flex justify-between p-4 items-center">
+        <h1 className="text-2xl font-bold">MOST POPULAR</h1>
+        <Link href="/populer" className="md:text-xl text-md underline hover:text-indigo-500 transition-all">Show All</Link>
+      </div>
+      <div className="grid md:grid-cols-4 sm:grid-cols-3 grid-cols-2 gap-4">
       {anime.data.map(data => {
-      return <AnimeList key={data.mal_id} title={data.title} images={data.images.webp.image_url}/>
+      return (
+        <div key={data.mal_id} className="shadow-xl">
+         <AnimeList title={data.title} images={data.images.webp.image_url} id={data.mal_id}/>
+         </div>      
+        )
       })}
       </div>
     </div>
