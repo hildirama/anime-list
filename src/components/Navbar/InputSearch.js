15db451484d858a396ed60ba92f333ca2cbe1215
hildirama <hildirama123@gmail.com>
2025-08-15 20:07:22 +0700

    add icon

--- src/components/Navbar/InputSearch.js
+++ src/components/Navbar/InputSearch.js
@@ -1,10 +1,17 @@
 "use client"
 
+import { MagnifyingGlass } from "@phosphor-icons/react"
+import { useRef } from "react"
+
 const InputSearch = () => {
+  const searchRef = useRef ()
   return (
-    <div>
+    <div className="relative">
         <input placeholder="search anime..." 
-        className=""/>
+        className="w-full p-2 rounded"/>
+        <button className="absolute top-2 end-2">
+          <MagnifyingGlass size={24} />
+        </button>
     </div>
 
   )
