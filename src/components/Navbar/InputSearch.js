1b33ec7069e9e0cd90810a3ced84b53e8cbad49e
hildirama <hildirama123@gmail.com>
2025-08-17 17:25:57 +0700

    inputsearch

--- src/components/Navbar/InputSearch.js
+++ src/components/Navbar/InputSearch.js
@@ -1,18 +1,26 @@
 "use client"
 
 import { MagnifyingGlass } from "@phosphor-icons/react"
+import { useRouter } from "next/navigation"
 import { useRef } from "react"
 
 const InputSearch = () => {
   const searchRef = useRef ()
+  const router = useRouter ()
+  const handleSearch = (event) => {
+    event.preventDefault()
+    const keyword = searchRef.current.value
+    router.push(`/search/${keyword}`)
+  }
   return (
-    <div className="relative">
+    <form className="relative">
         <input placeholder="search anime..." 
-        className="w-full p-2 rounded"/>
-        <button className="absolute top-2 end-2">
+        className="w-full p-2 rounded"
+        ref={searchRef}/>
+        <button type="submit" className="absolute top-2 end-2" onClick={handleSearch}>
           <MagnifyingGlass size={24} />
         </button>
-    </div>
+    </form>
 
   )
 }
